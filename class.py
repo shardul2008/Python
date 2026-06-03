feaafaab155efe987cff6d49cceffde718d1e9da
Shardul <shardulkarande42@gmail.com>
2026-06-03 09:55:47 +0530

    class

---
+++ class.py
@@ -0,0 +1,20 @@
+'''class demo:
+    welcome = "Welcome to Python"
+obj = demo()
+print(obj.welcome)  '''  
+
+class demo:
+    def show(self):
+        print("Welcome to Python")
+    welcome = "Welcome to Python"
+    name = "iGap Technologies"
+    city = "Kolhapur"
+    mobileno = "8767811254"
+    print("Name:", name)
+    print("City:", city)
+    print("Mobile No:", mobileno)
+
+
+obj = demo()
+obj.show()
+
