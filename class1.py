77aaf8ce670d2f6026eb4bd6b4e8e9c7732169ff
Shardul <shardulkarande42@gmail.com>
2026-06-03 10:13:44 +0530

    class1

--- class1.py
+++ class1.py
@@ -1,7 +1,21 @@
-class Demo:
+'''class Demo:
     def show(self,name,city,mobileno):
         print("name:",name)
         print("City:",city)
         print("Mobileno:",mobileno)
 obj = Demo()
-obj.show("iGap Technologies","Kolhapur","8767811254")
+obj.show("iGap Technologies","Kolhapur","8767811254") '''
+
+class Demo:
+    def __init__(self,name,city,mobileno):
+        self.name = name
+        self.city = city
+        self.mobileno = mobileno   
+    
+    def show(self):
+        print("name:",self.name)
+        print("City:",self.city)
+        print("Mobileno:",self.mobileno)
+
+obj = Demo("iGap","Kolhapur","5565246486")
+obj.show()
