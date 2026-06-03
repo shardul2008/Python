4483d81c71bfa1fc87d93185cf26729a6f17e3b9
Shardul <shardulkarande42@gmail.com>
2026-06-03 10:01:05 +0530

    class1

---
+++ class1.py
@@ -0,0 +1,7 @@
+class Demo:
+    def show(self,name,city,mobileno):
+        print("name:",name)
+        print("City:",city)
+        print("Mobileno:",mobileno)
+obj = Demo()
+obj.show("iGap Technologies","Kolhapur","8767811254")
